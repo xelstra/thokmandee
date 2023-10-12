d2e30dd7c3a3ef4b0c604cc3eca4f5b401a31003
xelstra <147712569+xelstra@users.noreply.github.com>
2023-10-12 17:23:30 +0500

    Updating files

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -22,46 +22,49 @@ const Sidebar = () => {
     }
 
     return (
-        <div className='categories p-2 mt-3'>
-            <ul className="list-group">
-                <li className='border-bottom'>Home</li>
-                <li className='border-bottom' onClick={() => handleMenu(1)}>Featured <span>{check(1) ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}</span></li>
-                <li className={check(1) ? '' : 'd-none'}>
-                    <div className='first-div'>
-                        <ul className='list-group border-bottom'>
-                            <li>Trending Collections</li>
-                            <li>Community</li>
-                            <li>Recommended</li>
-                        </ul>
+        <div className='categories bg1 p-2'>
+            <div className="d-flex justify-content-center gap-4">
+                <div >Home</div>
+                <div onClick={() => handleMenu(1)}>Featured <span>{check(1) ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}</span>
+                    <div className={check(1) ? '.' : 'd-none'}>
+                        <div className='first-div'>
+                            <div className=' '>
+                                <div>Trending Collections</div>
+                                <div>Community</div>
+                                <div>Recommended</div>
+                            </div>
+                        </div>
                     </div>
-                </li>
-                <li className='border-bottom'>New</li>
-                <li className='border-bottom'>Bestsellers</li>
-                <li className='border-bottom' onClick={() => handleMenu(2)}>Brands <span>{check(2) ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}</span></li>
-                <li className={check(2) ? '' : 'd-none'}>
-                    <div className='first-div'>
-                        <ul className='list-group border-bottom'>
-                            <li>Brands from Asia</li>
-                            <li>Brands from Europe</li>
-                            <li>Brands from Africa</li>
-                        </ul>
+                </div>
+                <div >New</div>
+                <div >Bestsellers</div>
+                <div onClick={() => handleMenu(2)}>Brands <span>{check(2) ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}</span>
+                    <div className={check(2) ? '' : 'd-none'}>
+                        <div className='first-div'>
+                            <div className=' '>
+                                <div>Brands from Asia</div>
+                                <div>Brands from Europe</div>
+                                <div>Brands from Africa</div>
+                            </div>
+                        </div>
                     </div>
-                </li>
-                <li className='border-bottom' onClick={() => handleMenu(3)}>Home Decor <span>{check(3) ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}</span></li>
-                <li className={check(3) ? '' : 'd-none'}>
-                    <div className='first-div'>
-                        <ul className='list-group w-100'>
-                            <li>Blankets & Duvets</li>
-                            <li>Pillows & Pillowcases</li>
-                            <li>Quitls & Coverlets</li>
-                            <li className='border-bottom'>Sheets</li>
-                        </ul>
+                </div>
+                <div onClick={() => handleMenu(3)}>Home Decor <span>{check(3) ? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown} />}</span>
+                    <div className={check(3) ? '' : 'd-none'}>
+                        <div className='first-div'>
+                            <div className=' w-100'>
+                                <div>Blankets & Duvets</div>
+                                <div>Pillows & Pillowcases</div>
+                                <div>Quitls & Coverlets</div>
+                                <div >Sheets</div>
+                            </div>
+                        </div>
                     </div>
-                </li>
-                <li className='border-bottom'>Food & Drink</li>
-                <li className='border-bottom'>Beauty & Wellness</li>
-                <li>Promotions</li>
-            </ul>
+                </div>
+                <div >Food & Drink</div>
+                <div >Beauty & Wellness</div>
+                <div>Promotions</div>
+            </div>
         </div>
     )
 }
